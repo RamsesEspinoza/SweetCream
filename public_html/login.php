9727a6eb8d7529a02113b588bbd43e6089992143
Brayan <Brayan@DESKTOP-OI1Q6PO>
2022-06-25 16:49:26 -0500

    CSS del login y registrar completos

--- public_html/login.php
+++ public_html/login.php
@@ -63,28 +63,96 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
+<!doctype html>
 <html>
     <head>
-        <title>Login</title>
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <link rel="stylesheet" href="css/sesion_style.css" type="text/css">
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>LOGIN</title>
+        <link rel="stylesheet" href="css/EstiloLogin.css"/>
+        <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
+        <link href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' rel='stylesheet'>
+        <script type='text/javascript' src=''></script>
+        <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
+        <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
     </head>
-    <body>
-
-        <header>
-            <a href="/php-login">¡Bienvenido a Sweet Cream!</a>
-        </header>
-
-        <h1>Inicia Sesion</h1>        
-        <span>o <a href="signup.php">Crea una Cuenta</a></span>
-
-        <form  method="post">
-            <input type="text" name="email" placeholder="Ingresa tu correo electronico">
-            <input type="password" name="password" placeholder="Ingresa tu contraseña">
-            <input type="submit" value="Ingresar"></form>
-
-        <div class="hero__item set-bg" data-setbg="img/hero/hero-1.jpg">
-
-
+    <body oncontextmenu='return false' class='snippet-body'>
+        <div class="wrapper bg-white">
+            <div class="h2 text-center" >Sweet Cream</div>
+            <div class="h4 text-muted text-center pt-2">Ingresa tu Correo y Contreseña</div>
+            <form method="post" class="pt-3">
+                <div class="form-group py-2">
+                    <div class="input-field"> 
+                        <span class="far fa-user p-2"></span> 
+                        <input type="text" name="email" placeholder="Ingresa tu correo electronico">
+                    </div>
+                </div>
+                <div class="form-group py-1 pb-2">
+                    <div class="input-field"> 
+                        <span class="fas fa-lock p-2"></span> 
+                        <input type="password" name="password" placeholder="Ingresa tu contraseña">
+                    </div>
+                </div>
+                <div class="d-flex align-items-start">
+                    <!--<div class="remember"> 
+                        <label class="option text-muted"> Recuerdame <input type="radio" name="radio"> 
+                            <span class="checkmark"></span> 
+                        </label> 
+                    </div>-->
+                </div> 
+                <div class="text-center pt-3 text-muted"><a href="#" id="forgot">Olvidaste tu contraseña?</a></div>
+                <button class="btn btn-block text-center my-3" type="submit" value="Ingresar">Iniciar Sesion</button>
+                <div class="text-center pt-3 text-muted">No tienes Cuenta? <a href="signup.php">Registrate</a></div>
+                <div class="text-center pt-3 text-muted"><a href="index.php">Revisar sin Iniciar Sesion</a></div>
+            </form>
+        </div>
+        <script type='text/javascript'></script>
     </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<html>
+<!-- Css Styles 
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+<head>
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<!-- Css Styles 
+<link rel="stylesheet" href="css/EstiloLogin.css" type="text/css">
+</head>
+<body>
+
+<header>
+    <a href="index.php">¡Bienvenido a Sweet Cream!</a>
+</header>
+
+<h1>Inicia Sesion</h1>        
+<span>o <a href="signup.php">Crea una Cuenta</a></span>
+
+<form  method="post">
+    <input type="text" name="email" placeholder="Ingresa tu correo electronico">
+    <input type="password" name="password" placeholder="Ingresa tu contraseña">
+    <input type="submit" value="Ingresar">
+</form>
+
+<div class="hero__item set-bg" data-setbg="img/hero/hero-1.jpg">
+
+
+</body>
+</html>-->
--- public_html/signup.php
+++ public_html/signup.php
@@ -18,15 +18,73 @@
     
 ?>
 
+<!doctype html>
 <html>
     <head>
-        <title>Registrate</title>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>LOGIN</title>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <link rel="stylesheet" href="css/sesion_style.css" type="text/css">
-
+        <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
+        <link href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' rel='stylesheet'>
+        <script type='text/javascript' src=''></script>
+        <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
+        <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
+        <link rel="stylesheet" href="css/EstiloLogin.css"/>
     </head>
-    <body>
-        
+    <body oncontextmenu='return false' class='snippet-body'>
+        <div class="wrapper bg-white">
+            <div class="h2 text-center" >¡Bienvenido a Sweet Cream!</div>
+            <div class="h4 text-muted text-center pt-2">Registrate</div>
+            
+            <form method="post" class="pt-3">
+                <div class="form-group py-2">
+                    <div class="input-field"> 
+                        <span class="far fa-user p-2"></span> 
+                        <input type="text" name="nombre" placeholder="Ingresa tu Nombre">
+                    </div>
+                </div>
+                <div class="form-group py-2">
+                    <div class="input-field"> 
+                        <span class="far fa-address-card p-2"></span> 
+                        <input type="text" name="email" placeholder="Ingresa tu correo electronico">
+                    </div>
+                </div>
+                <div class="form-group py-2">
+                    <div class="input-field"> 
+                        <span class="far fa-eye p-2"></span> 
+                        <input type="password" name="password" placeholder="Ingresa tu contraseña">
+                    </div>
+                </div>
+                <div class="form-group py-2">
+                    <div class="input-field"> 
+                        <span class="far fa-eye-slash p-2"></span> 
+                        <input type="password" name="confirm_password" placeholder="Confirma tu contraseña">
+                    </div>
+                </div>
+                <button class="btn btn-block text-center my-3" type="submit" value="Registrarme">Registrarme</button>
+                <div class="text-center pt-3 text-muted"><span>o<a href="login.php">Inicia Sesion</a></span></div>
+                
+                
+            </form>
+        </div>
+    </body>
+</html>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <!--<body>
         <header>
             <a href="/php-login">¡Bienvenido a Sweet Cream!</a>
         </header>
@@ -52,4 +110,4 @@
         
 
     </body>
-</html>
+</html>-->
